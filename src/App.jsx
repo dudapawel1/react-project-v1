e303064c1a86ef9ba7a75ace96ad01986844db3f
Paweł <dudapawel1@proton.me>
2023-09-14 01:28:27 +0200

    feat: Navigation of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { HashRouter as Router} from 'react-router-dom';
 
+import AsideMenu from './components/AsideMenu/AsideMenu';
 import Header from './components/Header/Header';
 import StoreProvider from './store/StoreProvider';
 
@@ -8,6 +10,11 @@ import './App.scss';
 const App = () => (
     <StoreProvider>
     <Header />
+    <Router>
+    <div className='content-wrapper'>
+        <AsideMenu />
+    </div>
+    </Router>
     </StoreProvider>
 );
 
