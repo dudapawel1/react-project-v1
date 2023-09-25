4ebcea0ff6ff03ee6d9e555b316a3e9696431819
Paweł <dudapawel1@proton.me>
2023-09-25 15:05:46 +0200

    fix: Wrong styles for navigation and header

--- src/components/AsideMenu/AsideMenu.jsx
+++ src/components/AsideMenu/AsideMenu.jsx
@@ -20,8 +20,10 @@ const AsideMenu = () => {
 
     return ( 
         <section className={style()}>
-            <UserMenu isUserLogged={Boolean(user)}/> 
-            {adminMenuComponent}
+            <div className={style('nav-wrapper')}>
+                <UserMenu isUserLogged={Boolean(user)}/> 
+                {adminMenuComponent}
+            </div>           
         </section>
      );
 }
