acb08ecd63cb5314d2b72a5ba05a9bf8fd2df37e
Paweł <dudapawel1@proton.me>
2023-09-26 19:55:32 +0200

    fix: Lack of product title

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -41,7 +41,7 @@ const Course = ({ authors, id, img, isUserContext = false, price, title }) => {
     return ( 
         <li>
             <article className={style()}>
-                <h3 className={style('title')}></h3>
+                <h3 className={style('title')}>{title}</h3>
                 <img alt={title} className={style('image')} src={img}/>
                 <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
                 <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
